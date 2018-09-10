0dbecd254ab5203e80feceb98d3c128a04c6c924
Fredrik Sjöberg <fredrik.sjoberg@webstep.se>
2018-09-10 12:57:37 +0200

    Added Xcode project

---
+++ ExposureStreamrootIntegration/ExposureStreamrootIntegration.h
@@ -0,0 +1,19 @@
+//
+//  ExposureStreamrootIntegration.h
+//  ExposureStreamrootIntegration
+//
+//  Created by Fredrik Sjöberg on 2018-09-10.
+//  Copyright © 2018 emp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ExposureStreamrootIntegration.
+FOUNDATION_EXPORT double ExposureStreamrootIntegrationVersionNumber;
+
+//! Project version string for ExposureStreamrootIntegration.
+FOUNDATION_EXPORT const unsigned char ExposureStreamrootIntegrationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ExposureStreamrootIntegration/PublicHeader.h>
+
+
